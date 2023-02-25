490713192900356637130cc719f98f1f9e45cbe1
Dungnq <nguyen.quoc.dung@sun-asterisk.com>
2023-02-25 19:41:42 +0700

    bai tap js lan 1

---
+++ javascript.js
@@ -0,0 +1,63 @@
+var array1 = new Array(5);
+var array2 = [
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  1,
+  "string",
+  true,
+  "phan tu cuoi",
+];
+// console.log(array2.length);
+
+// console.log(array2[array2.length - 1]);
+// array2[index];
+
+for (index = 0; index < array2.length; index++) {
+  document.write(array2[index] + "-");
+}
